f044214f96f43fbead9b74c60a37807a9ab2feb5
kosadmin <info@koutsourcing.vn>
2026-01-22 13:21:56 +0700

    Create masterData.ts

---
+++ src/constants/masterData.ts
@@ -0,0 +1,36 @@
+// src/constants/masterData.ts
+
+export const MASTER_DATA = {
+  // Danh sách dự án
+  projects: [
+    "Dự án Samsung",
+    "Dự án LG",
+    "Dự án Foxconn",
+    "Dự án Luxshare",
+    "Dự án Goertek"
+  ],
+  
+  // Danh sách tỉnh thành (Bạn có thể thêm đầy đủ 63 tỉnh thành vào đây)
+  cities: [
+    "Hà Nội",
+    "TP. Hồ Chí Minh",
+    "Bắc Ninh",
+    "Bắc Giang",
+    "Thái Nguyên",
+    "Hải Phòng",
+    "Đà Nẵng",
+    "Bình Dương",
+    "Đồng Nai"
+  ],
+
+  // Danh sách bộ phận tạo nguồn
+  sourceDepartments: [
+    "Phòng Tuyển dụng 1",
+    "Phòng Tuyển dụng 2",
+    "Cộng tác viên",
+    "Đại lý nguồn"
+  ],
+
+  // Giới tính (để đồng nhất)
+  genders: ["Nam", "Nữ", "Khác"]
+};
