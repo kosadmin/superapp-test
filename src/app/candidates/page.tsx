907a14f08f0fa9e96bccdf84a47dbec5fec24045
kosadmin <info@koutsourcing.vn>
2026-04-05 14:03:31 +0700

    Update page.tsx

--- src/app/candidates/page.tsx
+++ src/app/candidates/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import AppLayout from '@/components/AppLayout';
 import ProtectedRoute from '@/components/ProtectedRoute';
